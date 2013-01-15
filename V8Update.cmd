f8e1993077abf6c445aa0b6cf74a6a776c2ee6f7
ClearScript <ClearScript@microsoft.com>
2013-01-15 16:14:46 -0500

    V8Update enhancements.

--- V8Update.cmd
+++ V8Update.cmd
@@ -1,5 +1,14 @@
 @echo off
+setlocal
 
+if "%v8rev%"=="" goto LatestRev
+set v8revarg=-r %v8rev%
+goto SetMode
+:LatestRev
+set v8rev=Latest
+
+:SetMode
+echo V8 revision: %v8rev%
 set mode=%1
 if "%mode%"=="" goto ReleaseMode
 if /i "%mode%"=="debug" goto DebugMode
@@ -22,12 +31,13 @@ if not exist build\ goto CreateBuildDir
 echo Removing old build directory ...
 rd /s /q build
 :CreateBuildDir
+echo Creating build directory ...
 md build
-if errorlevel 1 goto End
+if errorlevel 1 goto Error
 cd build
 
 echo Downloading V8 ...
-svn checkout http://v8.googlecode.com/svn/trunk/ v8 >nul
+svn checkout %v8revarg% http://v8.googlecode.com/svn/trunk/ v8 >nul
 if errorlevel 1 goto Error1
 cd v8
 
@@ -55,9 +65,9 @@ if errorlevel 1 goto Error1
 xcopy v8\*.* v8-ia32\ /e /y >nul
 if errorlevel 1 goto Error1
 cd v8-ia32
-third_party\python_26\python build\gyp_v8 -Dtarget_arch=ia32 -Dcomponent=shared_library -Dv8_use_snapshot=false >nul
+third_party\python_26\python build\gyp_v8 -Dtarget_arch=ia32 -Dcomponent=shared_library -Dv8_use_snapshot=false >gyp.log
 if errorlevel 1 goto Error2
-devenv /build "%mode%|Win32" tools\gyp\v8.sln >nul
+devenv /build "%mode%|Win32" tools\gyp\v8.sln >build.log
 if errorlevel 1 goto Error2
 cd ..
 
@@ -67,9 +77,9 @@ if errorlevel 1 goto Error1
 xcopy v8\*.* v8-x64\ /e /y >nul
 if errorlevel 1 goto Error1
 cd v8-x64
-third_party\python_26\python build\gyp_v8 -Dtarget_arch=x64 -Dcomponent=shared_library -Dv8_use_snapshot=false >nul
+third_party\python_26\python build\gyp_v8 -Dtarget_arch=x64 -Dcomponent=shared_library -Dv8_use_snapshot=false >gyp.log
 if errorlevel 1 goto Error2
-devenv /build "%mode%|x64" tools\gyp\v8.sln >nul
+devenv /build "%mode%|x64" tools\gyp\v8.sln >build.log
 if errorlevel 1 goto Error2
 cd ..\..
 
@@ -77,31 +87,33 @@ if not exist lib\ goto CreateLibDir
 echo Removing old lib directory ...
 rd /s /q lib
 :CreateLibDir
+echo Creating lib directory ...
 md lib
-if errorlevel 1 goto End
+if errorlevel 1 goto Error
 
 if not exist include\ goto CreateIncludeDir
 echo Removing old include directory ...
 rd /s /q include
 :CreateIncludeDir
+echo Creating include directory ...
 md include
-if errorlevel 1 goto End
+if errorlevel 1 goto Error
 
 echo Updating ClearScript ...
 copy build\v8-ia32\build\%mode%\v8-ia32.dll lib\ >nul
-if errorlevel 1 goto End
+if errorlevel 1 goto Error
 copy build\v8-ia32\build\%mode%\v8-ia32.pdb lib\ >nul
-if errorlevel 1 goto End
+if errorlevel 1 goto Error
 copy build\v8-ia32\build\%mode%\lib\v8-ia32.lib lib\ >nul
-if errorlevel 1 goto End
+if errorlevel 1 goto Error
 copy build\v8-x64\build\%mode%\v8-x64.dll lib\ >nul
-if errorlevel 1 goto End
+if errorlevel 1 goto Error
 copy build\v8-x64\build\%mode%\v8-x64.pdb lib\ >nul
-if errorlevel 1 goto End
+if errorlevel 1 goto Error
 copy build\v8-x64\build\%mode%\lib\v8-x64.lib lib\ >nul
-if errorlevel 1 goto End
+if errorlevel 1 goto Error
 copy build\v8\include\*.* include\ >nul
-if errorlevel 1 goto End
+if errorlevel 1 goto Error
 copy build\v8\V8Patch.txt .\ >nul
 goto End
 
@@ -109,8 +121,11 @@ goto End
 cd ..
 :Error1
 cd ..
+:Error
+echo *** THE PREVIOUS STEP FAILED ***
 
 :End
 cd ..\..\..
 
 :Exit
+endlocal
