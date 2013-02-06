d9c1a5a6ebe34e23b3d4c82475134896ad2485df
ClearScript <ClearScript@microsoft.com>
2013-02-05 22:37:56 -0500

    Fix ClearScriptV8-xx build errors related to spaces in paths. Bump version to 5.0.2.

--- ClearScriptTest/Properties/AssemblyInfo.cs
+++ ClearScriptTest/Properties/AssemblyInfo.cs
@@ -69,5 +69,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("© Microsoft Corporation")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("5.0.1.0")]
-[assembly: AssemblyFileVersion("5.0.1.0")]
+[assembly: AssemblyVersion("5.0.2.0")]
+[assembly: AssemblyFileVersion("5.0.2.0")]
