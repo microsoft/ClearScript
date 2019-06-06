ba71cb02a01810df56e8c209c9d8048c71575de0
ClearScript <ClearScript@microsoft.com>
2019-06-05 21:02:32 -0400

    Updated JavaScript test files.

--- ClearScriptTest/JavaScript/Geometry/Geometry.js
+++ ClearScriptTest/JavaScript/Geometry/Geometry.js
@@ -1,20 +1,20 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license.
 
 import * as Arithmetic from '../Arithmetic/Arithmetic.js';
 
 export class Rectangle {
-	constructor(width, height) {
-		this.width = width;
-		this.height = height;
-	}
-	get Area() {
-		return Arithmetic.Multiply(this.width, this.height);
-	}
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    get Area() {
+        return Arithmetic.Multiply(this.width, this.height);
+    }
 }
 
 export class Square extends Rectangle {
-	constructor(side) {
-		super(side, side);
-	}
+    constructor(side) {
+        super(side, side);
+    }
 };
