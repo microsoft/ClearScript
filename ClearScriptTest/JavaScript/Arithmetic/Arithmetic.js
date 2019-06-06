f00d69ce37187a9d141fff9d7490cf296d4e7208
ClearScript <ClearScript@microsoft.com>
2019-06-05 20:37:25 -0400

    Added JavaScript test files.

---
+++ ClearScriptTest/JavaScript/Arithmetic/Arithmetic.js
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+export function Add(a, b) {
+	return a + b;
+}
+
+export function Subtract(a, b) {
+	return a - b;
+}
+
+export function Multiply(a, b) {
+	return a * b;
+}
+
+export function Divide(a, b) {
+	return a / b;
+}
