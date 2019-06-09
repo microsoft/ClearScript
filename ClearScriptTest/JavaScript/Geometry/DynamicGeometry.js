73b5ce2a203dcae8bfafcefdcfa1a0a391055ba6
ClearScript <ClearScript@microsoft.com>
2019-06-09 14:03:36 -0400

    Updated JavaScript test files.

---
+++ ClearScriptTest/JavaScript/Geometry/DynamicGeometry.js
@@ -0,0 +1,23 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+let Arithmetic;
+(async function() {
+    Arithmetic = await import('../Arithmetic/Arithmetic.js');
+})();
+
+export class Rectangle {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    get Area() {
+        return Arithmetic.Multiply(this.width, this.height);
+    }
+}
+
+export class Square extends Rectangle {
+    constructor(side) {
+        super(side, side);
+    }
+};
