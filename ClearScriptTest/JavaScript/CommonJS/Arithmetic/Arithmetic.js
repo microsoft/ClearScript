b8b7c38e67102190a4b9f55eff6f71f6b6d2caae
ClearScript <ClearScript@microsoft.com>
2019-07-12 15:51:08 -0400

    Updated JavaScript test files.

---
+++ ClearScriptTest/JavaScript/CommonJS/Arithmetic/Arithmetic.js
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+// ReSharper disable once PossiblyUnassignedProperty
+exports.Meta = module.getContext();
+
+exports.Add = function (a, b) {
+    return a + b;
+}
+
+exports.Subtract = function (a, b) {
+    return a - b;
+}
+
+exports.Multiply = function (a, b) {
+    return a * b;
+}
+
+exports.Divide = function (a, b) {
+    return a / b;
+}
