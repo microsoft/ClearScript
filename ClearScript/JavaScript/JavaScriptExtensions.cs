c1ccdfaf86f240255d5c1f39bafd6d4faf5a6beb
Kenneth Ellested <kenneth@ellested.dk>
2020-05-18 22:02:46 +0200

    Improved async error handling, including tests for the most general use cases, covering both Task and Task<T>

--- ClearScript/JavaScript/JavaScriptExtensions.cs
+++ ClearScript/JavaScript/JavaScriptExtensions.cs
@@ -1,102 +1,124 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-
-using System.Threading.Tasks;
 using Microsoft.ClearScript.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Microsoft.ClearScript.JavaScript
+namespace Microsoft.ClearScript.Test
 {
-    /// <summary>
-    /// Defines extension methods for use with JavaScript engines.
-    /// </summary>
-    public static class JavaScriptExtensions
-    {
-        private delegate void Executor(dynamic resolve, dynamic reject);
+	/// <summary>
+	/// Defines extension methods for use with JavaScript engines.
+	/// </summary>
+	public static class JavaScriptExtensions
+	{
+		private delegate void Executor(dynamic resolve, dynamic reject);
 
-        /// <summary>
-        /// Converts a <see cref="Task{TResult}"/> instance to a
-        /// <see href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise">promise</see>
-        /// for use with script code currently running on the calling thread.
-        /// </summary>
-        /// <typeparam name="TResult">The task's result type.</typeparam>
-        /// <param name="task">The task to convert to a promise.</param>
-        /// <returns>A promise that represents the task's asynchronous operation.</returns>
-        public static object ToPromise<TResult>(this Task<TResult> task)
-        {
-            return task.ToPromise(ScriptEngine.Current);
-        }
+		/// <summary>
+		/// Converts a <see cref="Task{TResult}"/> instance to a
+		/// <see href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise">promise</see>
+		/// for use with script code currently running on the calling thread.
+		/// </summary>
+		/// <typeparam name="TResult">The task's result type.</typeparam>
+		/// <param name="task">The task to convert to a promise.</param>
+		/// <returns>A promise that represents the task's asynchronous operation.</returns>
+		public static object ToPromise<TResult>(this Task<TResult> task)
+		{
+			return task.ToPromise(ScriptEngine.Current);
+		}
 
-        /// <summary>
-        /// Converts a <see cref="Task{TResult}"/> instance to a
-        /// <see href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise">promise</see>
-        /// for use with script code running in the specified script engine.
-        /// </summary>
-        /// <typeparam name="TResult">The task's result type.</typeparam>
-        /// <param name="task">The task to convert to a promise.</param>
-        /// <param name="engine">The script engine in which the promise will be used.</param>
-        /// <returns>A promise that represents the task's asynchronous operation.</returns>
-        public static object ToPromise<TResult>(this Task<TResult> task, ScriptEngine engine)
-        {
-            MiscHelpers.VerifyNonNullArgument(task, "task");
-            MiscHelpers.VerifyNonNullArgument(engine, "engine");
+		/// <summary>
+		/// Converts a <see cref="Task{TResult}"/> instance to a
+		/// <see href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise">promise</see>
+		/// for use with script code running in the specified script engine.
+		/// </summary>
+		/// <typeparam name="TResult">The task's result type.</typeparam>
+		/// <param name="task">The task to convert to a promise.</param>
+		/// <param name="engine">The script engine in which the promise will be used.</param>
+		/// <returns>A promise that represents the task's asynchronous operation.</returns>
+		public static object ToPromise<TResult>(this Task<TResult> task, ScriptEngine engine)
+		{
+			MiscHelpers.VerifyNonNullArgument(task, "task");
+			MiscHelpers.VerifyNonNullArgument(engine, "engine");
 
-            var ctor = (ScriptObject)engine.Script.Promise;
-            return ctor.Invoke(true, new Executor((resolve, reject) =>
-            {
-                task.ContinueWith(thisTask =>
-                {
-                    if (thisTask.IsCompleted)
-                    {
-                        resolve(thisTask.Result);
-                    }
-                    else
-                    {
-                        reject(thisTask.Exception);
-                    }
-                }, TaskContinuationOptions.ExecuteSynchronously);
-            }));
-        }
+			var ctor = (ScriptObject)engine.Script.Promise;
+			return ctor.Invoke(true, new Executor((resolve, reject) =>
+			{
+				task.ContinueWith(thisTask =>
+				{
+					if (thisTask.IsCanceled) reject(thisTask);
+					else if (thisTask.IsFaulted) reject(thisTask);
+					else if (thisTask.IsCompleted) resolve(thisTask.Result);
+					else reject(thisTask);
+				}, TaskContinuationOptions.ExecuteSynchronously);
+			}));
+		}
 
-        /// <summary>
-        /// Converts a <see cref="Task"/> instance to a
-        /// <see href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise">promise</see>
-        /// for use with script code currently running on the calling thread.
-        /// </summary>
-        /// <param name="task">The task to convert to a promise.</param>
-        /// <returns>A promise that represents the task's asynchronous operation.</returns>
-        public static object ToPromise(this Task task)
-        {
-            return task.ToPromise(ScriptEngine.Current);
-        }
+		/// <summary>
+		/// Converts a <see cref="Task"/> instance to a
+		/// <see href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise">promise</see>
+		/// for use with script code currently running on the calling thread.
+		/// </summary>
+		/// <param name="task">The task to convert to a promise.</param>
+		/// <returns>A promise that represents the task's asynchronous operation.</returns>
+		public static object ToPromise(this Task task)
+		{
+			return task.ToPromise(ScriptEngine.Current);
+		}
 
-        /// <summary>
-        /// Converts a <see cref="Task"/> instance to a
-        /// <see href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise">promise</see>
-        /// for use with script code running in the specified script engine.
-        /// </summary>
-        /// <param name="task">The task to convert to a promise.</param>
-        /// <param name="engine">The script engine in which the promise will be used.</param>
-        /// <returns>A promise that represents the task's asynchronous operation.</returns>
-        public static object ToPromise(this Task task, ScriptEngine engine)
-        {
-            MiscHelpers.VerifyNonNullArgument(task, "task");
-            MiscHelpers.VerifyNonNullArgument(engine, "engine");
+		/// <summary>
+		/// Converts a <see cref="Task"/> instance to a
+		/// <see href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise">promise</see>
+		/// for use with script code running in the specified script engine.
+		/// </summary>
+		/// <param name="task">The task to convert to a promise.</param>
+		/// <param name="engine">The script engine in which the promise will be used.</param>
+		/// <returns>A promise that represents the task's asynchronous operation.</returns>
+		public static object ToPromise(this Task task, ScriptEngine engine)
+		{
+			MiscHelpers.VerifyNonNullArgument(task, "task");
+			MiscHelpers.VerifyNonNullArgument(engine, "engine");
 
-            var ctor = (ScriptObject)engine.Script.Promise;
-            return ctor.Invoke(true, new Executor((resolve, reject) =>
-            {
-                task.ContinueWith(thisTask =>
-                {
-                    if (thisTask.IsCompleted)
-                    {
-                        resolve();
-                    }
-                    else
-                    {
-                        reject(thisTask.Exception);
-                    }
-                }, TaskContinuationOptions.ExecuteSynchronously);
-            }));
-        }
-    }
+			var ctor = (ScriptObject)engine.Script.Promise;
+			return ctor.Invoke(true, new Executor((resolve, reject) =>
+			{
+				task.ContinueWith(thisTask =>
+				{
+					if (thisTask.IsCanceled) reject(thisTask);
+					else if (thisTask.IsFaulted) reject(thisTask);
+					else if (thisTask.IsCompleted) resolve();
+					else reject(thisTask);
+				}, TaskContinuationOptions.ExecuteSynchronously);
+			}));
+		}
+
+
+		/// <summary>
+		/// Converts a JavaScript promise instance into a Task
+		/// <see href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise">promise</see>
+		/// </summary>
+		/// <param name="promise">The promise to convert into a Task.</param>
+		/// <returns>A Task that represents the Promise's asynchronous operation.</returns>
+		public static Task<object> ToTask(this object promise)
+		{
+			var source = new TaskCompletionSource<object>();
+			((dynamic)promise).then(
+				new Action<object>(result => source.SetResult(result)),
+				new Action<dynamic>(rejected =>
+				{
+					var t = rejected as Task;
+					if (t != null)
+					{
+						if (t.IsCanceled) source.SetCanceled();
+						else if (t.Exception != null) source.SetException(t.Exception);
+					}
+					else
+					{
+						source.SetException(new Exception(rejected.ToString()));
+					}
+				})
+			);
+			return source.Task;
+		}
+
+	}
 }
---
+++ ClearScriptTest/ToTaskExtensionTest.cs
@@ -0,0 +1,392 @@
+﻿using Microsoft.ClearScript.V8;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading.Tasks;
+
+namespace Microsoft.ClearScript.Test
+{
+    [TestClass]
+    [DeploymentItem("ClearScriptV8-64.dll")]
+    [DeploymentItem("ClearScriptV8-32.dll")]
+    [DeploymentItem("v8-x64.dll")]
+    [DeploymentItem("v8-ia32.dll")]
+    [DeploymentItem("v8-base-x64.dll")]
+    [DeploymentItem("v8-base-ia32.dll")]
+    [DeploymentItem("v8-zlib-x64.dll")]
+    [DeploymentItem("v8-zlib-ia32.dll")]
+    [SuppressMessage("Microsoft.Design", "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable", Justification = "Test classes use TestCleanupAttribute for deterministic teardown.")]
+    public class ToTaskExtensionTest : ClearScriptTest
+    {
+        #region setup / teardown
+
+        private ScriptEngine _engine;
+        private TestTasks _testTasks;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _engine = new V8ScriptEngine(V8ScriptEngineFlags.EnableDebugging);
+            _engine.AddHostType(typeof(Extensions));
+            _engine.AddHostType(typeof(JavaScriptExtensions));
+            _engine.AddHostObject("Tracer", new Tracer());
+            _testTasks = new TestTasks(_engine.Script.Tracer);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _engine.Dispose();
+            BaseTestCleanup();
+        }
+
+        #endregion
+
+        #region test methods
+
+        [TestMethod, TestCategory("ToTask")]
+        public async Task ToTask_HandlesException_ThrowBeforeReturningPromise__WithJavaScriptExceptionHandler()
+        {
+            var setupScript = @"
+				var Test = {
+					throwBeforePromise: async function() {
+						try {
+							throw 'Throw before promise error';
+						} catch (error) {
+							Tracer.Log('JS Exception: ' + error);
+							throw error;
+						}
+					}
+				}
+			";
+            var evaluateFunction = "Test.throwBeforePromise();";
+            var expectedTrace = new[] {
+                "JS Exception: Throw before promise error",
+                "CLR Exception: Throw before promise error"
+            };
+
+            await RunTestScript(setupScript, evaluateFunction, expectedTrace);
+        }
+
+        [TestMethod, TestCategory("ToTask")]
+        public async Task ToTask_HandlesException_ThrowInNonAsyncFunction()
+        {
+            var setupScript = @"
+				var Test = {
+					throwBeforePromise: function() {
+						throw 'Throw in non async function';
+					}
+				}
+			";
+            var evaluateFunction = "Test.throwBeforePromise();";
+            var expectedTrace = new[] {
+                "CLR Exception: Throw in non async function"
+            };
+
+            await RunTestScript(setupScript, evaluateFunction, expectedTrace);
+        }
+
+        [TestMethod, TestCategory("ToTask")]
+        public async Task ToTask_HandlesException_ThrowInNonAsyncFunction_WithJavaScriptExceptionHandler()
+        {
+            var setupScript = @"
+				var Test = {
+					throwBeforePromise: function() {
+						try {
+							throw 'Throw before promise error';
+						} catch (error) {
+							Tracer.Log('JS Exception: ' + error);
+							throw error;
+						}
+					}
+				}
+			";
+            var evaluateFunction = "Test.throwBeforePromise();";
+            var expectedTrace = new[] {
+                "JS Exception: Throw before promise error",
+                "CLR Exception: Throw before promise error"
+            };
+
+            await RunTestScript(setupScript, evaluateFunction, expectedTrace);
+        }
+
+        [TestMethod, TestCategory("ToTask")]
+        public async Task ToTask_HandlesExceptions_IntTask()
+        {
+            await RunTestScript(_engine,
+                _testTasks.GetIntTask(PointOfFailure.Canceled),
+                new[] { "A", "CLR Exception: A task was canceled." });
+            await RunTestScript(_engine,
+                _testTasks.GetIntTask(PointOfFailure.A),
+                new[] { "A", "CLR Exception: GetInt Fail A" });
+            await RunTestScript(_engine,
+                _testTasks.GetIntTask(PointOfFailure.B),
+                new[] { "A", "B", "CLR Exception: GetInt Fail B" });
+            await RunTestScript(_engine,
+                _testTasks.GetIntTask(PointOfFailure.C),
+                new[] { "A", "B", "C", "CLR Exception: GetInt Fail C" });
+        }
+
+        [TestMethod, TestCategory("ToTask")]
+        public async Task ToTask_HandlesExceptions_IntTask_WithJavaScriptTryCatch()
+        {
+            await RunTestScriptWithTryCatch(_engine,
+                _testTasks.GetIntTask(PointOfFailure.Canceled),
+                new[] { "A", "JS Exception: [object HostObject]", "CLR Exception: A task was canceled." });
+            await RunTestScriptWithTryCatch(_engine,
+                _testTasks.GetIntTask(PointOfFailure.A),
+                new[] { "A", "JS Exception: [object HostObject]", "CLR Exception: GetInt Fail A" });
+            await RunTestScriptWithTryCatch(_engine,
+                _testTasks.GetIntTask(PointOfFailure.B),
+                new[] { "A", "B", "JS Exception: [object HostObject]", "CLR Exception: GetInt Fail B" });
+            await RunTestScriptWithTryCatch(_engine,
+                _testTasks.GetIntTask(PointOfFailure.C),
+                new[] { "A", "B", "C", "JS Exception: [object HostObject]", "CLR Exception: GetInt Fail C" });
+        }
+
+        [TestMethod, TestCategory("ToTask")]
+        public async Task ToTask_HandlesExceptions_VoidTask()
+        {
+            await RunTestScript(_engine,
+                _testTasks.GetVoidTask(PointOfFailure.Canceled),
+                new[] { "A", "CLR Exception: A task was canceled." });
+            await RunTestScript(_engine,
+                _testTasks.GetVoidTask(PointOfFailure.A),
+                new[] { "A", "CLR Exception: GetVoid Fail A" });
+            await RunTestScript(_engine,
+                _testTasks.GetVoidTask(PointOfFailure.B),
+                new[] { "A", "B", "CLR Exception: GetVoid Fail B" });
+            await RunTestScript(_engine,
+                _testTasks.GetVoidTask(PointOfFailure.C),
+                new[] { "A", "B", "C", "CLR Exception: GetVoid Fail C" });
+        }
+
+        [TestMethod, TestCategory("ToTask")]
+        public async Task ToTask_HandlesExceptions_VoidTask_WithJavaScriptTryCatch()
+        {
+            await RunTestScriptWithTryCatch(_engine,
+                _testTasks.GetVoidTask(PointOfFailure.Canceled),
+                new[] { "A", "JS Exception: [object HostObject]", "CLR Exception: A task was canceled." });
+            await RunTestScriptWithTryCatch(_engine,
+                _testTasks.GetVoidTask(PointOfFailure.A),
+                new[] { "A", "JS Exception: [object HostObject]", "CLR Exception: GetVoid Fail A" });
+            await RunTestScriptWithTryCatch(_engine,
+                _testTasks.GetVoidTask(PointOfFailure.B),
+                new[] { "A", "B", "JS Exception: [object HostObject]", "CLR Exception: GetVoid Fail B" });
+            await RunTestScriptWithTryCatch(_engine,
+                _testTasks.GetVoidTask(PointOfFailure.C),
+                new[] { "A", "B", "C", "JS Exception: [object HostObject]", "CLR Exception: GetVoid Fail C" });
+        }
+
+        [TestMethod, TestCategory("ToTask")]
+        public async Task ToTask_ReturnsValue()
+        {
+            await RunTestScriptWithTryCatch(_engine,
+                _testTasks.GetVoidTask(PointOfFailure.None),
+                new[] { "A", "B", "C", "CLR Result: [undefined]" });
+        }
+
+        [TestMethod, TestCategory("ToTask")]
+        public async Task ToTask_ReturnsValue_IntTask()
+        {
+            await RunTestScript(_engine,
+                _testTasks.GetIntTask(PointOfFailure.None),
+                new[] { "A", "B", "C", "CLR Result: 10" });
+        }
+
+        [TestMethod, TestCategory("ToTask")]
+        public async Task ToTask_ReturnsValue_IntTask_WithJavaScriptTryCatch()
+        {
+            await RunTestScriptWithTryCatch(_engine,
+                _testTasks.GetIntTask(PointOfFailure.None),
+                new[] { "A", "B", "C", "CLR Result: 10" });
+        }
+
+        [TestMethod, TestCategory("ToTask")]
+        public async Task ToTask_ReturnsValue_InvokingNonAsyncFunction()
+        {
+            _engine.Script.testDelegate = _testTasks.GetIntTask(PointOfFailure.None);
+            var setupScript = @"
+				var Test = {
+					notAsync: function() {
+						return testDelegate().ToPromise();
+					}
+				}
+			";
+            var evaluateFunction = "Test.notAsync();";
+            var expectedTrace = new[] { "A", "B", "C", "CLR Result: 10" };
+            await RunTestScript(setupScript, evaluateFunction, expectedTrace);
+        }
+
+        [TestMethod, TestCategory("ToTask")]
+        public async Task ToTask_ReturnsValue_VoidTask()
+        {
+            await RunTestScript(_engine,
+                _testTasks.GetVoidTask(PointOfFailure.None),
+                new[] { "A", "B", "C", "CLR Result: [undefined]" });
+        }
+
+        [TestMethod, TestCategory("ToTask")]
+        public async Task ToTask_ReturnsValue_VoidTask_WithJavaScriptTryCatch()
+        {
+            await RunTestScriptWithTryCatch(_engine,
+                _testTasks.GetVoidTask(PointOfFailure.None),
+                new[] { "A", "B", "C", "CLR Result: [undefined]" });
+        }
+
+        private async Task RunTestScript(string setupScript, string evaluateFunction, string[] expectedTrace)
+        {
+            var expectedTraceString = string.Join(Environment.NewLine, expectedTrace);
+            await RunTestScript(setupScript, evaluateFunction, expectedTraceString);
+        }
+
+        private async Task RunTestScript(string setupScript, string evaluateFunction, string expectedTraceString)
+        {
+            _engine.Script.Tracer.Clear();
+            try
+            {
+                _engine.Execute(setupScript);
+                var result = await _engine.Evaluate(evaluateFunction).ToTask();
+                _engine.Script.Tracer.Log("CLR Result: " + result);
+            }
+            catch (Exception ex)
+            {
+                _engine.Script.Tracer.Log("CLR Exception: " + GetInnerMostMessage(ex));
+            }
+
+            Assert.AreEqual(expectedTraceString, _engine.Script.Tracer.GetTrace());
+        }
+
+        private async Task RunTestScript(ScriptEngine engine, Delegate testDelegate, string[] expectedTrace)
+        {
+            engine.Script.testDelegate = testDelegate;
+            var setupScript = @"
+				var Test = {
+					getDelegate: async function() {
+						return await testDelegate().ToPromise();
+					},
+				}
+			";
+            var evaluateFunction = "Test.getDelegate();";
+
+            await RunTestScript(setupScript, evaluateFunction, expectedTrace);
+        }
+
+        private async Task RunTestScriptWithTryCatch(ScriptEngine engine, Delegate testDelegate, string[] expectedTrace)
+        {
+            engine.Script.testDelegate = testDelegate;
+            var setupScript = @"
+				var Test = {
+					getDelegate: async function() {
+						try {
+							return await testDelegate().ToPromise();
+						} catch (error) {
+							Tracer.Log('JS Exception: ' + error);
+							throw error;
+						}
+					},
+				}
+			";
+            var evaluateFunction = "Test.getDelegate();";
+
+            await RunTestScript(setupScript, evaluateFunction, expectedTrace);
+        }
+
+        #endregion
+
+        #region miscelaneous
+
+        public enum PointOfFailure { None, A, B, C, Canceled };
+
+        public class Tracer
+        {
+            private List<string> _trace = new List<string>();
+
+            public void Clear()
+            {
+                _trace.Clear();
+            }
+
+            public string GetTrace()
+            {
+                return string.Join(Environment.NewLine, _trace);
+            }
+
+            public void Log(object traceMessage)
+            {
+                _trace.Add(traceMessage.ToString());
+            }
+        }
+
+        // The exception handling tests below are using these test tasks with three failure points named A,B,C
+        //  The point of failures are as follows:
+        //	A - As soon as the task is started
+        //  ... simulated work ...
+        //	B - In the middle of continuations
+        //  ... simulated work ...
+        //	C - At the end of the continuations
+        //	+ an extra simulating When it's canceled
+        // 	I did not split these into individual tests, as code changes will likely affect all at once. (or maybe I'm just too lazy)
+        private class TestTasks
+        {
+            private Tracer _tracer;
+
+            public TestTasks(Tracer tracer)
+            {
+                _tracer = tracer;
+            }
+
+            public async Task Delay()
+            {
+                await Task.Delay(5);
+            }
+
+            public async Task<int> GetInt(PointOfFailure pof)
+            {
+                _tracer.Log("A");
+                if (pof == PointOfFailure.A) throw new Exception("GetInt Fail A");
+                await Delay();
+                if (pof == PointOfFailure.Canceled) throw new OperationCanceledException();
+                _tracer.Log("B");
+                if (pof == PointOfFailure.B) throw new Exception("GetInt Fail B");
+                await Delay();
+                _tracer.Log("C");
+                if (pof == PointOfFailure.C) throw new Exception("GetInt Fail C");
+                return 10;
+            }
+
+            public Delegate GetIntTask(PointOfFailure pof)
+            {
+                return new Func<Task<int>>(() => GetInt(pof));
+            }
+
+            public async Task GetVoid(PointOfFailure pof)
+            {
+                _tracer.Log("A");
+                if (pof == PointOfFailure.A) throw new Exception("GetVoid Fail A");
+                await Delay();
+                if (pof == PointOfFailure.Canceled) throw new OperationCanceledException();
+                _tracer.Log("B");
+                if (pof == PointOfFailure.B) throw new Exception("GetVoid Fail B");
+                await Delay();
+                _tracer.Log("C");
+                if (pof == PointOfFailure.C) throw new Exception("GetVoid Fail C");
+            }
+
+            public Delegate GetVoidTask(PointOfFailure pof)
+            {
+                return new Func<Task>(() => GetVoid(pof));
+            }
+        }
+
+        public static string GetInnerMostMessage(Exception ex)
+        {
+            return ex.InnerException == null
+                ? ex.Message
+                : GetInnerMostMessage(ex.InnerException);
+        }
+
+        #endregion
+    }
+}
