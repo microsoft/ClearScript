ec824457605266b399d07a709cd7e96873c68895
William-Froelich <falash@gmail.com>
2021-12-18 00:32:51 -0600

    Preserve test project and docker environments for debugging

---
+++ MuslTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Microsoft.ClearScript.V8;
+
+namespace MuslTest
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var engine = new V8ScriptEngine();
+            engine.AddHostType("Console", typeof(Console));
+            engine.Evaluate("Console.WriteLine('Hello from JS')");
+        }
+    }
+}
